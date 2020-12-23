a3a24e98e9b51ad4aecdd4f1e6e6bcd7866a567f
Brandon Taylor <50997466+MrTwatWaffle@users.noreply.github.com>
2020-12-23 01:10:54 -0700

    Added PS3 Support

--- Filter.ps1
+++ Filter.ps1
@@ -3,7 +3,9 @@ gc temp.log| sort | get-unique > Graphics.txt
 Remove-Item temp.log
 
 (get-content Graphics.txt) | foreach-object {$_ -replace "rx2", "png "} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "http://skate3_web", " http://downloads.skate.online.ea.com"} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object { $_ -creplace "[^acdeghiklmnopstw0-9\s-:/.XBLOGO ]", "" } | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "---------- ", "Script By MrTwatWaffle ---> https://youtube.com/MrTwatWaffle"} | set-content Graphics.txt
+(get-content Graphics.txt) | foreach-object {$_ -replace "psg", "png "} | set-content Graphics.txt
+(get-content Graphics.txt) | foreach-object {$_ -replace "skate3_web", "downloads.skate.online.ea.com"} | set-content Graphics.txt
+(get-content Graphics.txt) | foreach-object {$_ -replace "http", " http"} | set-content Graphics.txt
+(get-content Graphics.txt) | foreach-object { $_ -creplace "[^acdeghiklmnopstwPS0-9\s-:/.XBLOGO ]", "" } | set-content Graphics.txt
+(get-content Graphics.txt) | foreach-object {$_ -replace "---------- ", "Script By MrTwatWaffle ---> https://youtube.com/MrTwatWaffle "} | set-content Graphics.txt
 Invoke-Item Graphics.txt
