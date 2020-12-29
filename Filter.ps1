d458815eb13f15fece01562d3c5022b7ddd4d989
Brandon Taylor <50997466+MrTwatWaffle@users.noreply.github.com>
2020-12-28 23:22:06 -0700

    Just Some Organization Fixes

--- Filter.ps1
+++ Filter.ps1
@@ -1,11 +1,18 @@
-gc temp.log| sort | get-unique > Graphics.txt
+gc temp.log| sort | get-unique > temp.txt
 
 Remove-Item temp.log
 
-(get-content Graphics.txt) | foreach-object {$_ -replace "rx2", "png "} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "psg", "png "} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "skate3_web", "downloads.skate.online.ea.com"} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "http", " http"} | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object { $_ -creplace "[^acdeghiklmnopstwPS0-9\s-:/.XBLOGO ]", "" } | set-content Graphics.txt
-(get-content Graphics.txt) | foreach-object {$_ -replace "---------- ", "Script By MrTwatWaffle ---> https://youtube.com/MrTwatWaffle "} | set-content Graphics.txt
+(get-content temp.txt) | foreach-object {$_ -replace "rx2", "!"} | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "psg", "!"} | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "http://skate3_web/skate3/content/XBL2/LOGO/", "@"} | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "http://downloads.skate.online.ea.com/skate3/content/PS3/LOGO/", "#"} | set-content temp.txt
+(get-content temp.txt) | foreach-object { $_ -creplace "[^!0-9#@/]", "" } | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "@", "http://downloads.skate.online.ea.com/skate3/content/XBL2/LOGO/"} | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "#", "http://downloads.skate.online.ea.com/skate3/content/PS3/LOGO/"} | set-content temp.txt
+(get-content temp.txt) | foreach-object {$_ -replace "!", ".png "} | set-content temp.txt
+
+gc temp.txt| sort | get-unique > Graphics.txt
+
 Invoke-Item Graphics.txt
+
+Remove-Item temp.txt
